a664926b60aa086c0af1d61332f3a5abb9214e08
alpercun <alper-aga97@hotmail.com>
2021-01-24 16:14:13 +0300

    Create an AG-Grid table

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,300 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="grid">
+    <div id="app">
+      <h1 class="header">Zeo Interview</h1>
+      <ag-grid-vue
+        style="width: 100%; height: 500px"
+        class="ag-theme-alpine"
+        :columnDefs="columnDefs"
+        :rowData="rowData"
+      ></ag-grid-vue>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { AgGridVue } from "ag-grid-vue";
 
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      columnDefs: null,
+      rowData: null,
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    AgGridVue,
+  },
+  beforeMount() {
+    this.columnDefs = [
+      {
+        headerName: "KEYWORDS",
+        field: "keywords",
+        sortable: true,
+        filter: true,
+        width: 200,
+      },
+      {
+        headerName: "SEARCH VOLUME",
+        field: "volume",
+        sortable: true,
+        filter: true,
+        width: 160,
+      },
+      {
+        headerName: "RANK",
+        field: "rank",
+        sortable: true,
+        filter: true,
+        width: 90,
+      },
+      {
+        headerName: "CHANGE",
+        field: "beforeChange",
+        sortable: true,
+        filter: true,
+        width: 120,
+      },
+      {
+        headerName: "PX RANK",
+        field: "pxrank",
+        sortable: true,
+        filter: true,
+        width: 120,
+      },
+      {
+        headerName: "CHANGE",
+        field: "afterChange",
+        sortable: true,
+        filter: true,
+        width: 105,
+      },
+      {
+        headerName: "URL-PAGE",
+        field: "url",
+        sortable: true,
+        filter: true,
+        width: 210,
+      },
+      {
+        headerName: "CPC - $",
+        field: "cpc",
+        sortable: true,
+        filter: true,
+        width: 120,
+      },
+    ];
+    this.rowData = [
+      {
+        keywords: "lorem ipsum dolar sit amet",
+        volume: "444.4k",
+        rank: "44",
+        beforeChange: "44",
+        pxrank: "44.4k",
+        afterChange: "44",
+        url: "/lorem-ipsum-dolar-sit-amet",
+        cpc: "44.44",
+      },
+      {
+        keywords: "lorem ipsum dolar sit amet",
+        volume: "444.4k",
+        rank: "44",
+        beforeChange: "44",
+        pxrank: "44.4k",
+        afterChange: "44",
+        url: "/lorem-ipsum-dolar-sit-amet",
+        cpc: "44.44",
+      },
+      {
+        keywords: "lorem ipsum dolar sit amet",
+        volume: "444.4k",
+        rank: "44",
+        beforeChange: "44",
+        pxrank: "44.4k",
+        afterChange: "44",
+        url: "/lorem-ipsum-dolar-sit-amet",
+        cpc: "44.44",
+      },
+      {
+        keywords: "lorem ipsum dolar sit amet",
+        volume: "444.4k",
+        rank: "44",
+        beforeChange: "44",
+        pxrank: "44.4k",
+        afterChange: "44",
+        url: "/lorem-ipsum-dolar-sit-amet",
+        cpc: "44.44",
+      },
+      {
+        keywords: "lorem ipsum dolar sit amet",
+        volume: "444.4k",
+        rank: "44",
+        beforeChange: "44",
+        pxrank: "44.4k",
+        afterChange: "44",
+        url: "/lorem-ipsum-dolar-sit-amet",
+        cpc: "44.44",
+      },
+      {
+        keywords: "lorem ipsum dolar sit amet",
+        volume: "444.4k",
+        rank: "44",
+        beforeChange: "44",
+        pxrank: "44.4k",
+        afterChange: "44",
+        url: "/lorem-ipsum-dolar-sit-amet",
+        cpc: "44.44",
+      },
+      {
+        keywords: "lorem ipsum dolar sit amet",
+        volume: "444.4k",
+        rank: "44",
+        beforeChange: "44",
+        pxrank: "44.4k",
+        afterChange: "44",
+        url: "/lorem-ipsum-dolar-sit-amet",
+        cpc: "44.44",
+      },
+      {
+        keywords: "lorem ipsum dolar sit amet",
+        volume: "444.4k",
+        rank: "44",
+        beforeChange: "44",
+        pxrank: "44.4k",
+        afterChange: "44",
+        url: "/lorem-ipsum-dolar-sit-amet",
+        cpc: "44.44",
+      },
+      {
+        keywords: "lorem ipsum dolar sit amet",
+        volume: "444.4k",
+        rank: "44",
+        beforeChange: "44",
+        pxrank: "44.4k",
+        afterChange: "44",
+        url: "/lorem-ipsum-dolar-sit-amet",
+        cpc: "44.44",
+      },
+      {
+        keywords: "lorem ipsum dolar sit amet",
+        volume: "444.4k",
+        rank: "44",
+        beforeChange: "44",
+        pxrank: "44.4k",
+        afterChange: "44",
+        url: "/lorem-ipsum-dolar-sit-amet",
+        cpc: "44.44",
+      },
+      {
+        keywords: "lorem ipsum dolar sit amet",
+        volume: "444.4k",
+        rank: "44",
+        beforeChange: "44",
+        pxrank: "44.4k",
+        afterChange: "44",
+        url: "/lorem-ipsum-dolar-sit-amet",
+        cpc: "44.44",
+      },
+      {
+        keywords: "lorem ipsum dolar sit amet",
+        volume: "444.4k",
+        rank: "44",
+        beforeChange: "44",
+        pxrank: "44.4k",
+        afterChange: "44",
+        url: "/lorem-ipsum-dolar-sit-amet",
+        cpc: "44.44",
+      },
+      {
+        keywords: "lorem ipsum dolar sit amet",
+        volume: "444.4k",
+        rank: "44",
+        beforeChange: "44",
+        pxrank: "44.4k",
+        afterChange: "44",
+        url: "/lorem-ipsum-dolar-sit-amet",
+        cpc: "44.44",
+      },
+      {
+        keywords: "lorem ipsum dolar sit amet",
+        volume: "444.4k",
+        rank: "44",
+        beforeChange: "44",
+        pxrank: "44.4k",
+        afterChange: "44",
+        url: "/lorem-ipsum-dolar-sit-amet",
+        cpc: "44.44",
+      },
+      {
+        keywords: "lorem ipsum dolar sit amet",
+        volume: "444.4k",
+        rank: "44",
+        beforeChange: "44",
+        pxrank: "44.4k",
+        afterChange: "44",
+        url: "/lorem-ipsum-dolar-sit-amet",
+        cpc: "44.44",
+      },
+      {
+        keywords: "lorem ipsum dolar sit amet",
+        volume: "444.4k",
+        rank: "44",
+        beforeChange: "44",
+        pxrank: "44.4k",
+        afterChange: "44",
+        url: "/lorem-ipsum-dolar-sit-amet",
+        cpc: "44.44",
+      },
+      {
+        keywords: "lorem ipsum dolar sit amet",
+        volume: "444.4k",
+        rank: "44",
+        beforeChange: "44",
+        pxrank: "44.4k",
+        afterChange: "44",
+        url: "/lorem-ipsum-dolar-sit-amet",
+        cpc: "44.44",
+      },
+      {
+        keywords: "lorem ipsum dolar sit amet",
+        volume: "444.4k",
+        rank: "44",
+        beforeChange: "44",
+        pxrank: "44.4k",
+        afterChange: "44",
+        url: "/lorem-ipsum-dolar-sit-amet",
+        cpc: "44.44",
+      },
+      {
+        keywords: "lorem ipsum dolar sit amet",
+        volume: "444.4k",
+        rank: "44",
+        beforeChange: "44",
+        pxrank: "44.4k",
+        afterChange: "44",
+        url: "/lorem-ipsum-dolar-sit-amet",
+        cpc: "44.44",
+      },
+      {
+        keywords: "lorem ipsum dolar sit amet",
+        volume: "444.4k",
+        rank: "44",
+        beforeChange: "44",
+        pxrank: "44.4k",
+        afterChange: "44",
+        url: "/lorem-ipsum-dolar-sit-amet",
+        cpc: "44.44",
+      },
+    ];
+  },
+};
 </script>
 
 <style lang="scss">
+@import "../node_modules/ag-grid-community/dist/styles/ag-grid.css";
+@import "../node_modules/ag-grid-community/dist/styles/ag-theme-alpine.css";
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
